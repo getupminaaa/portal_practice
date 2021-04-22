30a912b31161161d601f37f9d6fd039770e00a14
getupminaaa <getupmina0405@gmail.com>
2021-04-22 19:53:19 +0900

    07 종종 장애가 발생을 합니다. 장애 해결을 해보세요.

--- src/main/java/kr/ac/jejunu/userdao/UserDao.java
+++ src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -11,50 +11,71 @@ public class UserDao {
     }
 
     public User get(Integer id) throws ClassNotFoundException, SQLException {
-        //data 어딨지?
-        //mysql driver 로딩
-        Connection connection = dataSource.getConnection();
-        //query 만들고
-        PreparedStatement preparedStatement =
-                connection.prepareStatement("select * from portal where id = ?");
-        preparedStatement.setLong(1, id);
-        // query 실행하고
-        ResultSet resultSet = preparedStatement.executeQuery();
-        resultSet.next();
-        //결과를 User 잘 매핑하고
-        User user = new User();
-        user.setId(resultSet.getInt("id"));
-        user.setName(resultSet.getString("name"));
-        user.setPassword(resultSet.getString("password"));
-        //자원을 해지하고
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
-        //결과를 리턴
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet= null;
+        User user;
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = connection.prepareStatement("select * from portal where id = ?");
+            preparedStatement.setLong(1, id);
+            resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            user = new User();
+            user.setId(resultSet.getInt("id"));
+            user.setName(resultSet.getString("name"));
+            user.setPassword(resultSet.getString("password"));
+        } finally {
+            try {
+                resultSet.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+            try {
+                preparedStatement.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
         return user;
     }
 
     public void insert(User user) throws ClassNotFoundException, SQLException {
-        //data 어딨지?
-        //mysql driver 로딩
-        Connection connection = dataSource.getConnection();
-        //query 만들고
-        PreparedStatement preparedStatement =
-                connection.prepareStatement("insert into portal(name,password) value (?,?)",Statement.RETURN_GENERATED_KEYS);
-        preparedStatement.setString(1, user.getName());
-        preparedStatement.setString(2, user.getPassword());
-        preparedStatement.executeUpdate();
-        // query 실행하고
-        ResultSet resultSet = preparedStatement.getGeneratedKeys();
-        resultSet.next();
 
-        user.setId(resultSet.getInt(1));
-        //결과를 User 잘 매핑하고
-
-        //자원을 해지하고
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
-        //결과를 리턴
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = connection.prepareStatement("insert into portal(name,password) value (?,?)", Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.executeUpdate();
+            resultSet = preparedStatement.getGeneratedKeys();
+            resultSet.next();
+            user.setId(resultSet.getInt(1));
+        } finally {
+            try {
+                resultSet.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+            try {
+                preparedStatement.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
     }
 }
