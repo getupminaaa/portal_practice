a386a37fe7a9a0428673b7f022d8abcd4a1c14da
getupminaaa <getupmina0405@gmail.com>
2021-04-23 04:18:38 +0900

    Statement를 위한 Class생성이 너무 많아지고 있습니다. 클래스를 제거하고, 추상화된 인터페이스를 직접 구현해보세요.(Template Callback Pattern)

--- src/main/java/kr/ac/jejunu/userdao/UserDao.java
+++ src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -1,6 +1,9 @@
 package kr.ac.jejunu.userdao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class UserDao {
     private final JdbcContext jdbcContext;
@@ -10,23 +13,43 @@ public class UserDao {
     }
 
     public User get(Integer id) throws ClassNotFoundException, SQLException {
-        StatementStrategy statementStrategy = new GetStatementStrategy(id);
+        StatementStrategy statementStrategy = connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from portal where id = ?");
+            preparedStatement.setLong(1, id);
+            return preparedStatement;
+        };
         return jdbcContext.jdbcContextForGet(statementStrategy);
     }
 
     public void insert(User user) throws ClassNotFoundException, SQLException {
-        StatementStrategy statementStrategy = new InsertStatementStrategy(user);
+        StatementStrategy statementStrategy = connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement("insert into portal(name,password) value (?,?)", Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getPassword());
+            return preparedStatement;
+        };
 
         jdbcContext.jdbcConetextForInsert(user, statementStrategy);
     }
 
     public void update(User user) throws SQLException {
-        StatementStrategy statementStrategy = new UpdateStatementStrategy(user);
+        StatementStrategy statementStrategy = connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement("update portal set name = ?, password = ? where id =?");
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setInt(3, user.getId());
+            return preparedStatement;
+        };
         jdbcContext.jdbcConetextForUpdate(statementStrategy);
     }
 
     public void delete(Integer id) throws SQLException {
-        StatementStrategy statementStrategy = new DeleteStatementStrategy(id);
+        StatementStrategy statementStrategy = connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "delete from portal where id =?");
+            preparedStatement.setInt(1, id);
+            return preparedStatement;
+        };
         jdbcContext.jdbcConetextForUpdate(statementStrategy);
     }
 }
